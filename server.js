668a79a408f65d6e39b835b9f79a319d572f1700
rethanesh <rethanesh.s2025laids@sece.ac.in>
2025-12-18 09:40:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://coruscating-biscochitos-50ce0c.netlify.app'],
+  origin: true,
   credentials: true
 }));
 app.use(express.json());
