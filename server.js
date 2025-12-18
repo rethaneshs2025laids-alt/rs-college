8357015606bfecd74291f5154a484c874ee7b97f
rethanesh <rethanesh.s2025laids@sece.ac.in>
2025-12-18 09:25:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:5173', 'https://coruscating-biscochitos-50ce0c.netlify.app'],
+  credentials: true
+}));
 app.use(express.json());
 
 // MongoDB Connection
